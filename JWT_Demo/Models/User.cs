b51aeb70874ea797d70cb99eae0745e797aeb980
Maha.Saeed <maha.saeed@lebara.com>
2022-01-07 18:44:59 +0500

    Add project files.

---
+++ JWT_Demo/Models/User.cs
@@ -0,0 +1,26 @@
+﻿namespace JWT_Demo.Models
+{
+    public class User
+    {
+        public string UserName
+        {
+            get;
+            set;
+        }
+        public Guid Id
+        {
+            get;
+            set;
+        }
+        public string EmailId
+        {
+            get;
+            set;
+        }
+        public string Password
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ JWT_Demo/Models/UserLogin.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JWT_Demo.Models
+{
+    public class UserLogin
+    {
+        [Required]
+        public string UserName
+        {
+            get;
+            set;
+        }
+        [Required]
+        public string Password
+        {
+            get;
+            set;
+        }
+        public UserLogin() { }
+    }
+}
